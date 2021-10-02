355288de8eb868fee8832b82a82a9bb87fac4550
ndokas99 <54295411+ndokas99@users.noreply.github.com>
2021-10-02 17:59:23 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,218 @@
+from flask import render_template, flash, request, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import *
+from os import path
+from datetime import datetime, timedelta
+
+
+@app.route('/')
+def index():
+    session['state'] = 'logged out'
+    return render_template('index.html')
+
+
+@app.route('/signup')
+def signup():
+    session['state'] = 'logged out'
+    return render_template('signup.html')
+
+
+@app.route('/login')
+def login():
+    session['state'] = 'logged out'
+    return render_template('login.html')
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    if request.method == 'POST':
+        idNum = request.form['idNum']
+        idNum = idNum.replace(" ", "").replace("-", "")
+        fullname = request.form['fullName']
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+
+        if len(idNum) < 9:
+            flash("ID number is invalid", category="error")
+        elif Account.query.filter_by(id=idNum).first():
+            flash("Account with this ID already exists", category='error')
+        elif fullname.count(" ") < 1:
+            flash("Fullname should contain at least two words", category="error")
+        elif password1 != password2:
+            flash("Passwords do not match", category="error")
+        elif len(password1) < 7:
+            flash("Passwords should be at least 7 characters", category="error")
+        else:
+            acc = Account(
+                id=idNum,
+                fullname=fullname,
+                password=generate_password_hash(password1, method="sha256")
+            )
+            db.session.add(acc)
+            db.session.commit()
+            flash("Account created successfully.", category="success")
+            print('hello')
+            return render_template('login.html')
+
+        return render_template('signup.html')
+
+
+@app.route('/apply', methods=['POST'])
+def apply():
+    if request.method == 'POST':
+        idNum = request.form['idNum']
+        idNum = idNum.replace(" ", "").replace("-", "")
+        password = request.form['password']
+
+        acc = Account.query.filter_by(id=idNum).first()
+        if acc:
+            if check_password_hash(acc.password, password):
+                session['state'] = "logged in"
+                return render_template("application_form.html")
+            else:
+                flash("Account does not exist", category="error")
+        else:
+            flash("Account does not exist", category="error")
+
+        return render_template("login.html")
+
+
+@app.route('/booking', methods=['POST'])
+def book():
+    if session == 'logged out':
+        return render_template('login.html')
+    else:
+        if request.method == 'POST':
+            session['id'] = request.form['id'].replace(" ", "").replace("-", "")
+            personal = Personal(
+                id=session['id'],
+                birth_entry_no=request.form['ben'],
+                surname=request.form['surname'],
+                other_names=request.form['other_names'],
+                maiden_name=request.form['maiden_name'],
+                name_changed=request.form['name_changed'],
+                details=request.form['details'],
+                sex=request.form['sex'],
+                marital_status=request.form['marital_status'],
+                dob=request.form['dob'],
+                place_of_birth=request.form['pob'],
+                country_of_birth=request.form['cob'],
+                height=request.form['height'],
+                colour_of_eyes=request.form['coe'],
+                colour_of_hair=request.form['coh'],
+                peculiarities=request.form['pecs'],
+                profession=request.form['prof'],
+                address=request.form['res'],
+                perm_residence=request.form['copr'],
+                telephone=request.form['tel']
+            )
+            db.session.add(personal)
+
+            mdetails = Woman_marriage_details(
+                id=session['id'],
+                date_of_marriage=request.form['dom'] or None,
+                husband_name=request.form['hfn'] or None,
+                place_married=request.form['pcm'] or None,
+                husbands_place=request.form['hpcb'] or None,
+                husband_citizenship=request.form['hc'] or None,
+                marr_or_div_number=request.form['mdon'] or None,
+                married_more=request.form['mmto'] or None
+            )
+            db.session.add(mdetails)
+
+            kin = Kin(
+                id=session['id'],
+                kin_name=request.form['kfn'],
+                kin_relation=request.form['krta'],
+                kin_address=request.form['kra'],
+                kin_telephone=request.form['ktel']
+            )
+            db.session.add(kin)
+
+            citizenship = Citizenship(
+                id=session['id'],
+                citizen_by=request.form['citizen_by'],
+                issued_document_before=request.form['idb'],
+                documents_number=request.form['ipn'] or None,
+                holding_foreign=request.form['hfp'],
+                foreign_number=request.form['fpn'] or None,
+                renounce_foreign=request.form['rfb'],
+                surrender_foreign=request.form['sfp'],
+                surrendered_number=request.form['sfpn'] or None,
+                date=request.form['ddate']
+            )
+            db.session.add(citizenship)
+
+            guardian = Guardian(
+                id=session['id'],
+                guardian_name=request.form['gfn'] or None,
+                guardian_id=request.form['gid'] or None,
+                relation=request.form['grtc'] or None,
+                date=request.form['gdate'] or None
+            )
+            db.session.add(guardian)
+
+            lost = Lost_passport(
+                id=session['id'],
+                lost_passport_no=request.form['lspn'] or None,
+                issued_at=request.form['lsit'] or None,
+                issued_date=request.form['lsid'] or None,
+                bearer_name=request.form['lsfn'] or None,
+                reason=request.form['lsreason'] or None
+            )
+            db.session.add(lost)
+
+            prev = Previous_marriage(
+                id=session['id'],
+                details=request.form['prev_details'] or None
+            )
+            db.session.add(prev)
+
+            docs = Documents(
+                id=session['id'],
+                bcert=request.files['bcert'].stream.read(),
+                nid=request.files['nat_id'].stream.read(),
+                fnid=request.files['fnid'].stream.read()
+            )
+            db.session.add(docs)
+            db.session.commit()
+
+            return render_template('booking.html')
+
+
+@app.route('/confirmation', methods=['POST'])
+def confirm():
+    if session['state'] == 'logged out':
+        return render_template('login.html')
+    else:
+        if request.method == "POST":
+            now = datetime.now()
+            if request.form['period'] == '24hrs':
+                date = now + timedelta(days=1)
+            elif request.form['period'] == '3days':
+                date = now + timedelta(days=3)
+            else:
+                date = now + timedelta(days=91)
+
+            booking = Booking(
+                id=session['id'],
+                release=request.form['period'],
+                date=date.date(),
+                pay_id=request.form['pay_id']
+            )
+            db.session.add(booking)
+            db.session.commit()
+
+            return render_template('confirmation.html', date=date)
+
+
+def create_database():
+    if not path.exists("citizens.db"):
+        db.create_all(app=app)
+
+
+create_database()
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
---
+++ models.py
@@ -0,0 +1,122 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class App(Flask):
+    def __init__(self, import_name):
+        super().__init__(import_name)
+        self.debug = False
+        self.config.update({
+            "SECRET_KEY": 'HGR85NG75BF84NG85BG95NF9',
+            "SQLALCHEMY_DATABASE_URI": 'sqlite:///citizens.db',
+            "SQLALCHEMY_TRACK_MODIFICATIONS": False
+        })
+
+
+app = App(__name__)
+db = SQLAlchemy(app)
+
+
+class Account(db.Model):
+    id = db.Column(db.Text, primary_key=True)
+    fullname = db.Column(db.Text, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+
+class Booking(db.Model):
+    id = db.Column(db.Text, primary_key=True)
+    release = db.Column(db.Text, nullable=False)
+    date = db.Column(db.Text, nullable=False)
+    pay_id = db.Column(db.Text, nullable=False)
+
+
+class Personal(db.Model):
+    id = db.Column(db.Text, primary_key=True)
+    birth_entry_no = db.Column(db.Text, nullable=False)
+    surname = db.Column(db.Text, nullable=False)
+    other_names = db.Column(db.Text, nullable=True)
+    maiden_name = db.Column(db.Text, nullable=True)
+    name_changed = db.Column(db.Text, nullable=False)
+    details = db.Column(db.Text, nullable=True)
+    sex = db.Column(db.Text, nullable=False)
+    marital_status = db.Column(db.Text, nullable=False)
+    dob = db.Column(db.Text, nullable=False)
+    place_of_birth = db.Column(db.Text, nullable=False)
+    country_of_birth = db.Column(db.Text, nullable=False)
+
+    height = db.Column(db.Text, nullable=False)
+    colour_of_eyes = db.Column(db.Text, nullable=False)
+    colour_of_hair = db.Column(db.Text, nullable=False)
+    peculiarities = db.Column(db.Text, nullable=True)
+    profession = db.Column(db.Text, nullable=False)
+    address = db.Column(db.Text, nullable=False)
+    perm_residence = db.Column(db.Text, nullable=False)
+    telephone = db.Column(db.Text, nullable=False)
+    db.relationship('Woman_marriage_details')
+    db.relationship('Kin')
+    db.relationship('Citizenship')
+    db.relationship('Guardian')
+    db.relationship('Lost_passport')
+    db.relationship('Previous_marriage')
+    db.relationship('Documents')
+
+
+class Woman_marriage_details(db.Model):
+    id = db.Column(db.Text, db.ForeignKey('personal.id'), primary_key=True)
+    date_of_marriage = db.Column(db.Text, nullable=True)
+    husband_name = db.Column(db.Text, nullable=True)
+    place_married = db.Column(db.Text, nullable=True)
+    husbands_place = db.Column(db.Text, nullable=True)
+    husband_citizenship = db.Column(db.Text, nullable=True)
+    marr_or_div_number = db.Column(db.Text, nullable=True)
+    married_more = db.Column(db.Text, nullable=True)
+
+
+class Kin(db.Model):
+    id = db.Column(db.Text, db.ForeignKey('personal.id'), primary_key=True)
+    kin_name = db.Column(db.Text, nullable=False)
+    kin_relation = db.Column(db.Text, nullable=False)
+    kin_address = db.Column(db.Text, nullable=False)
+    kin_telephone = db.Column(db.Text, nullable=False)
+
+
+class Citizenship(db.Model):
+    id = db.Column(db.Text, db.ForeignKey('personal.id'), primary_key=True)
+    citizen_by = db.Column(db.Text, nullable=False)
+    issued_document_before = db.Column(db.Text, nullable=False)
+    documents_number = db.Column(db.Text, nullable=True)
+    holding_foreign = db.Column(db.Text, nullable=False)
+    foreign_number = db.Column(db.Text, nullable=True)
+    renounce_foreign = db.Column(db.Text, nullable=False)
+    surrender_foreign = db.Column(db.Text, nullable=False)
+    surrendered_number = db.Column(db.Text, nullable=True)
+    date = db.Column(db.Text, nullable=False)
+
+
+class Guardian(db.Model):
+    id = db.Column(db.Text, db.ForeignKey('personal.id'), primary_key=True)
+    guardian_name = db.Column(db.Text, nullable=True)
+    guardian_id = db.Column(db.Text, nullable=True)
+    relation = db.Column(db.Text, nullable=True)
+    date = db.Column(db.Text, nullable=True)
+
+
+class Lost_passport(db.Model):
+    id = db.Column(db.Text, db.ForeignKey('personal.id'), primary_key=True)
+    lost_passport_no = db.Column(db.Integer, nullable=True)
+    issued_at = db.Column(db.Text, nullable=True)
+    issued_date = db.Column(db.Text, nullable=True)
+    bearer_name = db.Column(db.Text, nullable=True)
+    reason = db.Column(db.Text, nullable=True)
+
+
+class Previous_marriage(db.Model):
+    id = db.Column(db.Text, db.ForeignKey('personal.id'), primary_key=True)
+    details = db.Column(db.Text, nullable=True)
+
+
+class Documents(db.Model):
+    id = db.Column(db.Text, db.ForeignKey('personal.id'), primary_key=True)
+    bcert = db.Column(db.BLOB, nullable=False)
+    nid = db.Column(db.BLOB, nullable=False)
+    fnid = db.Column(db.BLOB, nullable=False)
